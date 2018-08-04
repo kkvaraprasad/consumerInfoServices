d114bf6d199bd8db741b62b40812dd67629d1450
F5033780 <F5033780@FNBUCSRGK72.fnb.co.za>
2018-08-04 23:53:57 +0200

    commit

---
+++ js/showDiv.js
@@ -0,0 +1,154 @@
+
+function hideAllDivs(){
+	document.getElementById("del_comp").style.display = "none";
+	document.getElementById("lenovo_comp").style.display = "none";
+	document.getElementById("samsung_phone").style.display = "none";
+	document.getElementById("apple_phone").style.display = "none";
+	document.getElementById("del_laptop").style.display = "none";
+	document.getElementById("lenovo_laptop").style.display = "none";
+	document.getElementById("mac_laptop").style.display = "none";
+	document.getElementById("canon_printer").style.display = "none";
+	document.getElementById("hp_printer").style.display = "none";
+	document.getElementById("hp_fax").style.display = "none";
+	document.getElementById("panasonic_fax").style.display = "none";
+	document.getElementById("canon_cam").style.display = "none";
+	document.getElementById("nikon_cam").style.display = "none";
+	document.getElementById("samsung_ws").style.display = "none";
+	document.getElementById("lg_ws").style.display = "none";
+	document.getElementById("samsung_tv").style.display = "none";
+	document.getElementById("lg_tv").style.display = "none";
+	document.getElementById("hisence_tv").style.display = "none";
+	document.getElementById("defy_fridge").style.display = "none";
+	document.getElementById("hisense_fridge").style.display = "none";
+	document.getElementById("samsung_fridge").style.display = "none";
+	document.getElementById("samsung_ac").style.display = "none";
+	document.getElementById("lg_ac").style.display = "none";
+	document.getElementById("panasonic_ac").style.display = "none";
+	document.getElementById("mixer").style.display = "none";
+	document.getElementById("grinder").style.display = "none";
+	document.getElementById("juicer").style.display = "none";
+	document.getElementById("bpMonitor").style.display = "none";
+	document.getElementById("sugurMonitor").style.display = "none";
+	document.getElementById("threadmill").style.display = "none";
+}
+
+function show_DelComp(){
+	hideAllDivs();
+	document.getElementById("del_comp").style.display = "block";	
+}
+function show_LenovoComp(){
+	hideAllDivs();
+	document.getElementById("lenovo_comp").style.display = "block";
+}
+function show_samsungPhone(){
+	hideAllDivs();
+	document.getElementById("samsung_phone").style.display = "block";
+}
+function show_applePhone(){
+	hideAllDivs();
+	document.getElementById("apple_phone").style.display = "block";
+}
+function show_del_laptop(){
+	hideAllDivs();
+	document.getElementById("del_laptop").style.display = "block";
+}
+function show_lenovo_laptop(){
+	hideAllDivs();
+	document.getElementById("lenovo_laptop").style.display = "block";
+}
+function show_mac_laptop(){
+	hideAllDivs();
+	document.getElementById("mac_laptop").style.display = "block";
+}
+function show_canon_printer(){
+	hideAllDivs();
+	document.getElementById("canon_printer").style.display = "block";
+}
+function show_hp_printer(){
+	hideAllDivs();
+	document.getElementById("hp_printer").style.display = "block";
+}
+function show_hpFax(){
+	hideAllDivs();
+	document.getElementById("hp_fax").style.display = "block";
+}
+function show_panasonicFax(){
+	hideAllDivs();
+	document.getElementById("panasonic_fax").style.display = "block";
+}
+function show_canon_cam(){
+	hideAllDivs();
+	document.getElementById("canon_cam").style.display = "block";
+}
+function show_nikon_cam(){
+	hideAllDivs();
+	document.getElementById("nikon_cam").style.display = "block";
+}
+function show_samsung_ws(){
+	hideAllDivs();
+	document.getElementById("samsung_ws").style.display = "block";
+}
+function show_lg_ws(){
+	hideAllDivs();
+	document.getElementById("lg_ws").style.display = "block";
+}
+function show_samsung_tv(){
+	hideAllDivs();
+	document.getElementById("samsung_tv").style.display = "block";
+}
+function show_lg_tv(){
+	hideAllDivs();
+	document.getElementById("lg_tv").style.display = "block";
+}
+function show_hisence_tv(){
+	hideAllDivs();
+	document.getElementById("hisence_tv").style.display = "block";
+}
+function show_defy_fridge(){
+	hideAllDivs();
+	document.getElementById("defy_fridge").style.display = "block";
+}
+function show_hisense_fridge(){
+	hideAllDivs();
+	document.getElementById("hisense_fridge").style.display = "block";
+}
+function show_samsung_fridge(){
+	hideAllDivs();
+	document.getElementById("samsung_fridge").style.display = "block";
+}
+function show_samsung_ac(){
+	hideAllDivs();
+	document.getElementById("samsung_ac").style.display = "block";
+}
+function show_lg_ac(){
+	hideAllDivs();
+	document.getElementById("lg_ac").style.display = "block";
+}
+function show_panaconic_ac(){
+	hideAllDivs();
+	document.getElementById("panasonic_ac").style.display = "block";
+}
+function show_mixed(){
+	hideAllDivs();
+	document.getElementById("mixer").style.display = "block";
+}
+function show_grinder(){
+	hideAllDivs();
+	document.getElementById("grinder").style.display = "block";
+}
+function show_juicer(){
+	hideAllDivs();
+	document.getElementById("juicer").style.display = "block";
+}
+function show_bpmonitor(){
+	hideAllDivs();
+	document.getElementById("bpMonitor").style.display = "block";
+}
+function show_sugermonitor(){
+	hideAllDivs();
+	document.getElementById("sugurMonitor").style.display = "block";
+}
+function show_threadmill(){
+	hideAllDivs();
+	document.getElementById("threadmill").style.display = "block";
+}
